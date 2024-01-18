3c60d64e84f89ae069f51c07a9b73d40955a9f2d
gagecates <gagecates10@gmail.com>
2024-01-17 19:35:06 -0600

    Remove unused footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/Footer";
 import SnowFall from "@/components/SnowFall";
 
 const inter = Inter({ subsets: ["latin"] });
